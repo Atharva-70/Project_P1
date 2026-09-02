b9d8b8c40aa848a0e049822cdfb48c1ca8c5efdf
Atharva-70 <mahaleatharva5@gmail.com>
2026-09-02 19:59:52 +0530

    another-email-check

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
     app.run(host = "0.0.0.0",port="5000", debug=True)
 
 #Nothing just for fun
-# Testing Jenkins email notification
+# Testing Jenkins email notification - 2
